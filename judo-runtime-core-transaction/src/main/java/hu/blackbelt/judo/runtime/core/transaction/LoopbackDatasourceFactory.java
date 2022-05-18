a4e4d3864e3c0196891594ef02a6dffe1dfbed19
Robert Csakany <csakany.robert@gmail.com>
2022-05-18 02:07:08 +0200

    Ubused transaction project removed

--- judo-runtime-core-transaction/src/main/java/hu/blackbelt/judo/runtime/core/transaction/LoopbackDatasourceFactory.java
+++
@@ -1,9 +0,0 @@
-package hu.blackbelt.judo.runtime.core.transaction;
-
-import javax.sql.DataSource;
-
-public class LoopbackDatasourceFactory implements ManagedDatasourceFactory {
-    public DataSource create(DataSource dataSource, String name) {
-        return dataSource;
-    }
-}
--- judo-runtime-core-transaction/src/main/java/hu/blackbelt/judo/runtime/core/transaction/ManagedDatasourceFactory.java
+++
@@ -1,7 +0,0 @@
-package hu.blackbelt.judo.runtime.core.transaction;
-
-import javax.sql.DataSource;
-
-public interface ManagedDatasourceFactory {
-   DataSource create(DataSource dataSource, String name);
-}
