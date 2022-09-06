5cb3b537d7904025302ca768ae2a2807dfc414b8
Norbert Herczeg <noherczeg@gmail.com>
2022-09-06 17:38:37 +0200

    JNG-3998: validator rollback

--- judo-runtime-core-validator/src/test/java/hu.blackbelt.judo.runtime.core.validator/PrecisionValidatorTest.java
+++ judo-runtime-core-validator/src/test/java/hu.blackbelt.judo.runtime.core.validator/PrecisionValidatorTest.java
@@ -19,63 +19,20 @@ public class PrecisionValidatorTest {
     void testValidateDecimal() {
         final EcorePackage ecore = EcorePackage.eINSTANCE;
 
-        final EPackage ePackage = newEPackageBuilder()
-                .withName("TestEpackage")
-                .withNsPrefix("test")
-                .withNsURI("http:///com.example.test.ecore")
-                .build();
-
-        final EAnnotation bigDecimalAttrAnnotation = newEAnnotationBuilder()
+        final EAnnotation doubleAnnotation = newEAnnotationBuilder()
                 .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/constraints")
                 .build();
-        bigDecimalAttrAnnotation.getDetails().put("precision", "64");
-        bigDecimalAttrAnnotation.getDetails().put("scale", "20");
-
-        final EAnnotation bigIntegerAttrAnnotation = newEAnnotationBuilder()
-                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/constraints")
-                .build();
-        bigIntegerAttrAnnotation.getDetails().put("precision", "18");
-
-        final EAnnotation javaMathBigIntegerAttrAnnotation = newEAnnotationBuilder()
-                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/constraints")
-                .build();
-        javaMathBigIntegerAttrAnnotation.getDetails().put("precision", "18");
-
-        final EAnnotation javaMathBigDecimalAttrAnnotation = newEAnnotationBuilder()
-                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/constraints")
-                .build();
-        javaMathBigDecimalAttrAnnotation.getDetails().put("precision", "64");
-        javaMathBigDecimalAttrAnnotation.getDetails().put("scale", "20");
+        doubleAnnotation.getDetails().put("precision", "16");
+        doubleAnnotation.getDetails().put("scale", "4");
 
         final EClass eClass = newEClassBuilder()
                 .withName("TestNumericTypesClass")
                 .withEStructuralFeatures(
                         ImmutableList.of(
                                 newEAttributeBuilder()
-                                        .withName("bigDecimalAttr")
-                                        .withEType(ecore.getEBigDecimal())
-                                        .withEAnnotations(bigDecimalAttrAnnotation)
-                                        .build(),
-                                newEAttributeBuilder()
-                                        .withName("bigInteger")
-                                        .withEType(ecore.getEBigInteger())
-                                        .withEAnnotations(bigIntegerAttrAnnotation)
-                                        .build(),
-                                newEAttributeBuilder()
                                         .withName("doubleAttr")
                                         .withEType(ecore.getEDouble())
-                                        .build(),
-                                newEAttributeBuilder()
-                                        .withName("floatAttr")
-                                        .withEType(ecore.getEFloat())
-                                        .build(),
-                                newEAttributeBuilder()
-                                        .withName("intAttr")
-                                        .withEType(ecore.getEInt())
-                                        .build(),
-                                newEAttributeBuilder()
-                                        .withName("longAttr")
-                                        .withEType(ecore.getELong())
+                                        .withEAnnotations(doubleAnnotation)
                                         .build()
                         )
                 )
@@ -83,11 +40,11 @@ public class PrecisionValidatorTest {
 
         PrecisionValidator validator = new PrecisionValidator();
         Map<String, Object> raw = new HashMap<String, Object>();
-        raw.put("TScaled", "123456");
+        raw.put("TScaled", Double.valueOf(123456));
         Payload payload = Payload.asPayload(raw);
 
-        Collection<ValidationResult> results = validator.validateValue(payload, eClass.getEStructuralFeature("bigDecimalAttr"), "123456", payload);
+        Collection<ValidationResult> results = validator.validateValue(payload, eClass.getEStructuralFeature("doubleAttr"), Double.valueOf(123456), payload);
 
-        assertEquals(2, results.size());
+        assertEquals(0, results.size());
     }
 }
