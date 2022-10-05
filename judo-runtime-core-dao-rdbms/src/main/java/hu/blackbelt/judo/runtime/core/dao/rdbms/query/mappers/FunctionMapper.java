530626c34b121094f80d68198cc13e1a19a0fc76
Bence Gelei <54545024+bencegelei@users.noreply.github.com>
2022-10-05 15:39:07 +0200

    JNG-3795 Support capitalize on strings (#47)
    * JNG-3795 Commented capitalize
    
    * JNG-3795 Version update
    
    * JNG-3795 Fool-proof substring
    
    * [Release] Updating versions

--- judo-runtime-core-dao-rdbms/src/main/java/hu/blackbelt/judo/runtime/core/dao/rdbms/query/mappers/FunctionMapper.java
+++ judo-runtime-core-dao-rdbms/src/main/java/hu/blackbelt/judo/runtime/core/dao/rdbms/query/mappers/FunctionMapper.java
@@ -192,7 +192,7 @@ public abstract class FunctionMapper<ID> extends RdbmsMapper<Function> {
                         .parameters(List.of(c.parameters.get(ParameterName.STRING))));
 
 //        functionBuilderMap.put(FunctionSignature.CAPITALIZE_STRING, c ->
-//                c.builder.pattern("(UPPER(SUBSTRING({0}, 1, 1)) || LOWER(SUBSTRING({0}, 2)))")
+//                c.builder.pattern("CAPITALIZE({0})")
 //                        .parameters(List.of(c.parameters.get(ParameterName.STRING))));
 
         functionBuilderMap.put(FunctionSignature.CONCATENATE_STRING, c ->
@@ -220,7 +220,10 @@ public abstract class FunctionMapper<ID> extends RdbmsMapper<Function> {
                         .parameters(List.of(c.parameters.get(ParameterName.STRING), c.parameters.get(ParameterName.PATTERN), c.parameters.get(ParameterName.REPLACEMENT))));
 
         functionBuilderMap.put(FunctionSignature.SUBSTRING_STRING, c ->
-                c.builder.pattern("SUBSTRING({0}, CAST ({1} AS INTEGER), CAST({2} AS INTEGER))")
+                c.builder.pattern("(CASE " +
+                                  "WHEN {1} > LENGTH({0}) THEN '''' " +
+                                  "ELSE SUBSTRING({0}, {1}, {2}) " +
+                                  "END)")
                         .parameters(List.of(c.parameters.get(ParameterName.STRING), c.parameters.get(ParameterName.POSITION), c.parameters.get(ParameterName.LENGTH))));
 
         functionBuilderMap.put(FunctionSignature.ADD_DATE, c ->
