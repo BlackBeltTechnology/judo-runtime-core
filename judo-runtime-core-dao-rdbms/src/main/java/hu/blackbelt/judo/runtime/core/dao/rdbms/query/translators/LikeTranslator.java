ef88632261857233607aae8a3778668f77550cc2
bencegelei <gelei.bence@gmail.com>
2022-10-20 19:32:06 +0200

    JNG-4169 Fix like translator

--- judo-runtime-core-dao-rdbms/src/main/java/hu/blackbelt/judo/runtime/core/dao/rdbms/query/translators/LikeTranslator.java
+++ judo-runtime-core-dao-rdbms/src/main/java/hu/blackbelt/judo/runtime/core/dao/rdbms/query/translators/LikeTranslator.java
@@ -9,18 +9,20 @@ package hu.blackbelt.judo.runtime.core.dao.rdbms.query.translators;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
 
-import hu.blackbelt.judo.meta.expression.*;
+import hu.blackbelt.judo.meta.expression.Expression;
+import hu.blackbelt.judo.meta.expression.StringExpression;
+import hu.blackbelt.judo.meta.expression.constant.StringConstant;
 import hu.blackbelt.judo.meta.expression.logical.Like;
 import lombok.Builder;
 import lombok.NonNull;
@@ -39,8 +41,9 @@ public class LikeTranslator implements Function<Like, Like> {
     public Like apply(final Like likeExpression) {
         return newLikeBuilder()
                 .withExpression((StringExpression) translator.apply(likeExpression.getExpression()))
-                .withPattern((StringExpression) translator.apply(likeExpression.getPattern()))
-                .withCaseInsensitive((LogicalExpression) translator.apply(likeExpression.getCaseInsensitive()))
+                .withPattern((StringConstant) translator.apply(likeExpression.getPattern()))
+                .withCaseInsensitive(likeExpression.isCaseInsensitive())
                 .build();
     }
+
 }
