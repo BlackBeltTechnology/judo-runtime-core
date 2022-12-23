a69375eee1128a22a8c3d5bc172840a8d07bd14c
Robert Csakany <csakany.robert@gmail.com>
2022-12-23 15:13:22 +0100

    JNG-4225 When model name is not defined trying to search model on classpath

--- judo-runtime-core-spring/src/main/java/hu/blackbelt/judo/runtime/core/spring/JudoModelLoaderConfiguration.java
+++ judo-runtime-core-spring/src/main/java/hu/blackbelt/judo/runtime/core/spring/JudoModelLoaderConfiguration.java
@@ -20,49 +20,51 @@ package hu.blackbelt.judo.runtime.core.spring;
  * #L%
  */
 
-import hu.blackbelt.judo.meta.asm.runtime.AsmModel;
-import hu.blackbelt.judo.meta.expression.runtime.ExpressionModel;
-import hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModel;
-import hu.blackbelt.judo.meta.measure.runtime.MeasureModel;
-import hu.blackbelt.judo.meta.rdbms.runtime.RdbmsModel;
-import hu.blackbelt.judo.meta.rdbms.support.RdbmsModelResourceSupport;
-import hu.blackbelt.judo.meta.rdbmsDataTypes.support.RdbmsDataTypesModelResourceSupport;
-import hu.blackbelt.judo.meta.rdbmsNameMapping.support.RdbmsNameMappingModelResourceSupport;
-import hu.blackbelt.judo.meta.rdbmsRules.support.RdbmsTableMappingRulesModelResourceSupport;
 import hu.blackbelt.judo.runtime.core.dao.rdbms.Dialect;
-import hu.blackbelt.judo.tatami.asm2rdbms.Asm2RdbmsTransformationTrace;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.SneakyThrows;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Enumeration;
-
-import static hu.blackbelt.judo.meta.asm.runtime.AsmModel.LoadArguments.asmLoadArgumentsBuilder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Configuration
+@Slf4j
 public class JudoModelLoaderConfiguration {
 
     @Autowired
     Dialect dialect;
 
-    @Value("${judo.modelName}")
+    @Value("${judo.modelName:}")
     private String modelName;
 
     @Bean
-    @ConditionalOnProperty(prefix = "judo", name = "modelName")
     public JudoModelLoader defaultJudoModelLoader() throws Exception {
+
+        if (modelName == null || modelName.isBlank()) {
+            log.info("Model name not set, searching...");
+            Pattern p = Pattern.compile( "(.*)-asm.model");
+            for (Resource resource: new PathMatchingResourcePatternResolver(this.getClass().getClassLoader())
+                    .getResources("classpath*:/model/*.model")){
+                Matcher m = p.matcher(resource.getFilename());
+                if (m.matches() ) {
+                    if (modelName != null && !modelName.isBlank()) {
+                        log.warn("Module already set, ignoring " + m.group(1) + ". To change set judo.modelName parameter " +
+                                "in application properties");
+                    }
+                    log.info("Model name found: " + modelName);
+                    modelName = m.group(1);
+                }
+            }
+        }
+        if (modelName == null || modelName.isBlank()) {
+            throw new IllegalArgumentException("modelName is not set and could not determinate");
+        }
+
         JudoModelLoader modelHolder = JudoModelLoader.
                 loadFromClassloader(modelName,
                         JudoModelLoader.class.getClassLoader(),
