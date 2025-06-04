42c76f5f8c8a2a035a496962e3c8678572b2dfde
Robert Csakany <csakany.robert@gmail.com>
2025-06-04 15:49:48 +0200

    JNG-6310 [Release] Updating versions

--- judo-runtime-core-guice-cxf/src/main/java/hu/blackbelt/judo/runtime/core/jaxrs/cxf/server/guice/CxfConfigurations.java
+++ judo-runtime-core-guice-cxf/src/main/java/hu/blackbelt/judo/runtime/core/jaxrs/cxf/server/guice/CxfConfigurations.java
@@ -1,84 +1,123 @@
 package hu.blackbelt.judo.runtime.core.jaxrs.cxf.server.guice;
 
+import com.google.inject.BindingAnnotation;
 import com.google.inject.Inject;
 
 import javax.annotation.Nullable;
 import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 public class CxfConfigurations {
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfJaxRsServerUrl {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfJaxRsServerPath {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfSkipDefaultJsonProviderRegistration {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfWadlServiceDescriptionAvailable {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfMetricsEnabled {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfLoggingEnabled {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfLogException {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfReturnRuntimeExceptions {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfIncludeBusinessCause {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfDefaultRequestContentType {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsAllowOrigin {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsAllowCredentials {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsAllowHeaders {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsExposeHeaders {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsMaxAge {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsPrefligthErrorStatus {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsBlockIfUnauthorized {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface CxfCorsDefaultOptionsMethodsHandlePreflight {}
 
 }
--- judo-runtime-core-guice-keycloak/src/main/java/hu/blackbelt/judo/runtime/core/security/keycloak/guice/KeycloakConfigurationQualifiers.java
+++ judo-runtime-core-guice-keycloak/src/main/java/hu/blackbelt/judo/runtime/core/security/keycloak/guice/KeycloakConfigurationQualifiers.java
@@ -1,97 +1,145 @@
 package hu.blackbelt.judo.runtime.core.security.keycloak.guice;
 
+import com.google.inject.BindingAnnotation;
+
 import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 public class KeycloakConfigurationQualifiers {
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakServerUrl {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakPublicUrl {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakAdminUser {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakAdminPassword {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakClientSecret {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerSupportLoginByEmail {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerClientAccessTypeForHuman {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerClientAccessTypeForSystem {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerCorsAllowOrigin {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerAsyncServiceCall {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerRetryMaxAttempts {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerRetryExponentialBackoff {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakRealmSynchronizerRetryWaitDuration {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakIdentityManagerIsReady {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerEnabled {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerUpdateExistingUsers {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerRequiredActions {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerAsyncServiceCall {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerRetryMaxAttempts {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerRetryExponentialBackoff {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakUserManagerRetryWaitDuration {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface KeycloakSecurityPasswordPolicyType {}
 
 
