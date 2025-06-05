26471ec851228d97e4347cb7e0a85a3103f50b9c
Robert Csakany <csakany.robert@gmail.com>
2025-06-05 21:12:26 +0200

    JNG-6310 Update epsilon (#243)
    [2025-06-05 20:04:52 +0200] JNG-6310 [Release] Updating versions
    [2025-06-05 08:53:05 +0200] Wait for usermanager 5 sec
    [2025-06-05 08:41:19 +0200] Fixing concurrent access
    [2025-06-05 08:19:38 +0200] Using executor to avoid java security problems
    [2025-06-04 22:31:59 +0200] JNG-6310 [Release] Updating versions
    [2025-06-04 19:13:35 +0200] JNG-6310 [Release] Updating versions
    [2025-06-04 15:49:48 +0200] JNG-6310 [Release] Updating versions
    [2025-06-03 15:05:20 +0200] Avoiding transient junit dependency leaks
    [2025-06-02 14:25:48 +0200] JNG-6310 [Release] Updating versions
    [2025-06-02 13:16:12 +0200] JNG-6310 [Release] Updating versions
    [2025-06-02 10:48:25 +0200] Update java versions 
    [2025-06-01 19:16:07 +0200] JNG-6310 Fixing Java 21 related versions
    [2025-05-29 00:22:30 +0200] JNG-6310 Update epsilon

--- judo-runtime-core-guice-cxf/src/main/java/hu/blackbelt/judo/runtime/core/jaxrs/cxf/server/guice/CxfQualifiers.java
+++ judo-runtime-core-guice-cxf/src/main/java/hu/blackbelt/judo/runtime/core/jaxrs/cxf/server/guice/CxfQualifiers.java
@@ -1,25 +1,37 @@
 package hu.blackbelt.judo.runtime.core.jaxrs.cxf.server.guice;
 
+import com.google.inject.BindingAnnotation;
+
 import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 public class CxfQualifiers {
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface InInterceptors {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface OutInterceptors {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface FaultInterceptors {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface Providers {}
 
 
--- judo-runtime-core-guice-jetty/src/main/java/hu/blackbelt/judo/runtime/core/jetty/guice/JettyConfigurations.java
+++ judo-runtime-core-guice-jetty/src/main/java/hu/blackbelt/judo/runtime/core/jetty/guice/JettyConfigurations.java
@@ -1,33 +1,46 @@
 package hu.blackbelt.judo.runtime.core.jetty.guice;
 
+import com.google.inject.BindingAnnotation;
 import com.google.inject.Inject;
 import lombok.Getter;
 
 import javax.annotation.Nullable;
 import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 public class JettyConfigurations {
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface JettyServerPort {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface JettyServerContextPath {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface JettyServerMaxThreads {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface JettyServerMinThreads {}
 
     @Qualifier
+    @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
     @Retention(RetentionPolicy.RUNTIME)
+    @BindingAnnotation
     public @interface JettyServerIdleTimeout {}
 
 }
